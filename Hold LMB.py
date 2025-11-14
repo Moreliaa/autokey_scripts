6fe87a0ee0cf37f89f802a2428b9dac1c3140425
Eric E. <e.e@e.e>
2025-11-14 23:55:42 +0100

    increase delay between key release and press for stability

--- Hold LMB.py
+++ Hold LMB.py
@@ -2,5 +2,5 @@ import time
 import random
 
 mouse.release_button(1)
-time.sleep(float(random.randrange(10, 20)) / 1000.0)
+time.sleep(float(random.randrange(100, 200)) / 1000.0)
 mouse.press_button(1)
